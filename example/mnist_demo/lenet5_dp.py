9649ab3d8d9fd45b9ae8228cb6ccd66bc12b4e27
ZhidanLiu <liuzhidan5@huawei.com>
2020-06-23 17:08:12 +0800

    update default value

--- example/mnist_demo/lenet5_dp.py
+++ example/mnist_demo/lenet5_dp.py
@@ -87,7 +87,7 @@ def generate_mnist_dataset(data_path, batch_size=32, repeat_size=1,
 
 if __name__ == "__main__":
     # This configure can run both in pynative mode and graph mode
-    context.set_context(mode=context.PYNATIVE_MODE, device_target=cfg.device_target)
+    context.set_context(mode=context.GRAPH_MODE, device_target=cfg.device_target)
     network = LeNet5()
     net_loss = nn.SoftmaxCrossEntropyWithLogits(is_grad=False, sparse=True, reduction="mean")
     config_ck = CheckpointConfig(save_checkpoint_steps=cfg.save_checkpoint_steps,
