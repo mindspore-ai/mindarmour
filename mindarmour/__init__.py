ae69cce14234f269dd696aaeb7d5e694b7bc481e
ZhidanLiu <liuzhidan5@huawei.com>
2022-12-12 15:27:55 +0800

    fix bug of version check

--- mindarmour/__init__.py
+++ mindarmour/__init__.py
@@ -15,6 +15,9 @@
 MindArmour, a tool box of MindSpore to enhance model trustworthiness and achieve
 privacy-preserving machine learning.
 """
+import logging
+import time
+
 from .adv_robustness.attacks.attack import Attack
 from .adv_robustness.attacks.black.black_model import BlackModel
 from .adv_robustness.defenses.defense import Defense
@@ -27,7 +30,6 @@ from .privacy.sup_privacy.train.model import SuppressModel
 from .privacy.sup_privacy.mask_monitor.masker import SuppressMasker
 from .privacy.evaluation.inversion_attack import ImageInversionAttack
 from .reliability.concept_drift.concept_drift_check_time_series import ConceptDriftCheckTimeSeries
-from .utils import logger
 
 __all__ = ['Attack',
            'BlackModel',
@@ -47,7 +49,7 @@ def _mindspore_version_check():
     """
     Do the MindSpore version check for MindArmour. If the
     MindSpore can not be imported, it will raise ImportError. If its
-    version is not compatibale with current MindArmour verision,
+    version is not compatible with current MindArmour version,
     it will print a warning.
 
     Raise:
@@ -56,17 +58,14 @@ def _mindspore_version_check():
     try:
         from mindarmour.version import __version__
         ma_version = __version__[:3]
-    except ModuleNotFoundError:
-        logger.warning(f"Get MindArmour version failed")
-        return
+    except (ImportError, ModuleNotFoundError):
+        raise ImportError(f"Get MindArmour version failed")
 
     try:
         import mindspore as ms
     except (ImportError, ModuleNotFoundError):
-        print("Can not find MindSpore in current environment. Please install "
-              "MindSpore before using MindSpore Reinforcement, by following "
-              "the instruction at https://www.mindspore.cn/install")
-        raise
+        raise ImportError("Can not find MindSpore in current environment. Please install MindSpore before " \
+                          "using MindArmour, by following  the instruction at https://www.mindspore.cn/install")
 
     ms_ma_version_match = {'1.7': ['1.7'],
                            '1.8': ['1.7', '1.8', '1.9', '2.0'],
@@ -77,16 +76,14 @@ def _mindspore_version_check():
     required_mindspore_verision = ms_ma_version_match.get(ma_version[:3])
 
     if ms_version not in required_mindspore_verision:
-        logger.warning("Current version of MindSpore is not compatible with MindArmour. "
-                       "Some functions might not work or even raise error. Please install MindSpore "
-                       "version in {}. For more details about dependency setting, please check "
-                       "the instructions at MindSpore official website https://www.mindspore.cn/install "
-                       "or check the README.md at https://gitee.com/mindspore/mindarmour".format(
-                           required_mindspore_verision))
+        logging.warning("Current version of MindSpore is not compatible with MindArmour. " \
+                        "Some functions might not work or even raise error. Please install MindSpore " \
+                        "version in %s. For more details about dependency setting, please check " \
+                        "the instructions at MindSpore official website https://www.mindspore.cn/install " \
+                        "or check the README.md at https://gitee.com/mindspore/mindarmour", required_mindspore_verision)
         warning_countdown = 3
         for i in range(warning_countdown, 0, -1):
-            logger.warning(
-                f"Please pay attention to the above warning, countdown: {i}")
+            logging.warning("Please pay attention to the above warning, countdown: %d .", i)
             time.sleep(1)
 
 
